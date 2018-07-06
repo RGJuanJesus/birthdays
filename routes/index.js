ff9ac45c3b3db24229bc5639c80f622fcb363e47
RGJuanJesus <40749041+RGJuanJesus@users.noreply.github.com>
2018-07-06 18:46:17 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,9 @@ router.get("/", function(req, res, next) {
       FROM
           "People"
       ORDER BY
-          EXTRACT(MONTH FROM birth_date)<10`,
-          /*IF(DATE_FORMAT(birth_date, "%m%d")-DATE_FORMAT(NOW(), "%m%d")<=0,
+          birth_date DESC`,
+          /*Heroku no soporta estas funciones pero el código está comprobado
+          IF(DATE_FORMAT(birth_date, "%m%d")-DATE_FORMAT(NOW(), "%m%d")<=0,
                DATE_FORMAT(birth_date, "%m%d")-DATE_FORMAT(NOW(), "%m%d")+1231,
                DATE_FORMAT(birth_date, "%m%d")-DATE_FORMAT(NOW(), "%m%d"))`,*/
       {
